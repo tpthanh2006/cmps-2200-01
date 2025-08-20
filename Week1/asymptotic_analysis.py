f34e3d93ee8df9aafdfd8e87e229b69cb7023f65
tpthanh2006 <tranpthanh2006@gmail.com>
2025-08-20 16:48:37 -0500

    feat: learn asymp analysis in week 1

--- Week1/asymptotic_analysis.py
+++ Week1/asymptotic_analysis.py
@@ -1,1 +1,19 @@
 # Asymptotic Analysis
+
+'''
+* Big Idea -> ignore machine-dependent constants
+
+* Asymptotic Dominance: Function f(n) dominates function g(n) if there exist constants c and n0 such that:
+      c * f(n) >= g(n) for all n >= n0
+'''
+
+def new_linear_search(mylist, key): # cost      number of times
+  for i in range(len(mylist)):      #  c5             n
+    if mylist[i] == key:            #  c6             n
+      return i                      #  c3             0 -> worst case: key not found
+      
+  return -1                         #  c4             1
+
+  # Cost(new-linear-search, n) = c5n + c6n + c4 = f1(n)
+
+
