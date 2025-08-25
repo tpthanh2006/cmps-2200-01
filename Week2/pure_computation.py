cc2d13c73d436ee9720ace79d9af027493d56b62
tpthanh2006 <tranpthanh2006@gmail.com>
2025-08-25 18:09:47 -0500

    learn about work & span in week 2.1

---
+++ Week2/pure_computation.py
@@ -0,0 +1,15 @@
+'''
+- No side effects -> Pure computation
+
+- Perfect speedup: dividing W work accross P processor yields total time (W / P)
+
+- work:
+  + T1 = time using 1 processor
+  + total energy consumed by a computation
+- span:
+  + Tinf = time using inf processors
+  + min. possible time that the computation requires
+
+- parallelism = T1 / Tinf -> maximum possible speedup with unlimited processors
+
+'''
